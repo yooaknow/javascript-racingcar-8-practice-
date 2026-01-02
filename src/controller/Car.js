1d708f39bd580f18c8671919829da11549dc6efa
yooaknow <dbdkfma1228@naver.com>
2026-01-02 22:13:40 +0900

    feat: 입력된 값이 5 이상이면 에러 던지기

--- src/controller/Car.js
+++ src/controller/Car.js
@@ -1,9 +1,21 @@
 class Car {
   constructor(name, position = 0) {
-    this.name = name;
-    this.position = position;
+
+    if (name.length>=5)
+    {
+      throw new Error("[Error] 입력된 값이 5보다 큽니다.");
+    }else{
+      this.name = name;
+      this.position = position;
+    }
+  }
+
+  run (num){
+    if (num>=4)
+    this.position += 1;
   }
 }
+
 
 const car = new Car("pobi");
 
@@ -11,4 +23,12 @@ console.log(car);
 console.log(car.name);     
 console.log(car.position);
 
+car.run(5);
+console.log(car.position);
+
+
 //클래스로 객체 생성 
+
+// 만약, 입력된 값이 5보다 크다면 에러 던지기
+const car2 = new Car("pobiiii");
+console.log(car2); 
