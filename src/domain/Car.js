e4a1b741981f7c71fcedbb93ebd2fdafda76cf88
yooaknow <dbdkfma1228@naver.com>
2026-01-02 23:17:42 +0900

    테스트 코드 확인

--- src/domain/Car.js
+++ src/domain/Car.js
@@ -2,24 +2,21 @@
 - 입력된 값이 5보다 크지 않으면 예외 처리 
 - 이름과 거리가 들어간 객체 생성
 - 만약 들어온 랜덤 값이 4보나 크거나 같은 경우에 더함
-
-
-- (추가해야함) 이름이 비어 있으면 에러
+- 이름이 비어 있으면 에러
 */
-
-
 
 class Car {
   constructor(name, position = 0) {
 
-    if (name.length>=5)
+    if (name.length >=5 || name.length === 0)
     {
-      throw new Error("[Error] 입력된 값이 5보다 큽니다.");
-    }else{
+      throw new Error("[Error] 이름은 1~5자이어야합니다.");
+    } 
+
       this.name = name;
       this.position = position;
-    }
-  }
+}
+  
 
   run (num){
     if (num>=4)
@@ -38,8 +35,18 @@ car.run(5);
 console.log(car.position);
 
 
-//클래스로 객체 생성 
+//Error 메시지 확인 
+const car1 = new Car("pobi");
+console.log(car1);
 
-// 만약, 입력된 값이 5보다 크다면 에러 던지기
-const car2 = new Car("pobiiii");
-console.log(car2); 
+try {
+  const car2 = new Car("pobiiii");
+} catch (e) {
+  console.log(e.message);
+}
+
+try {
+  const car3 = new Car("");
+} catch (e) {
+  console.log(e.message);
+}
